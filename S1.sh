121db103a0d063895656d9aaa798174962ce9730
SeraphinaShi <junmingshi67@gmail.com>
2024-01-23 11:24:05 -0800

    fix error in .sh files

--- S1.sh
+++ S1.sh
@@ -6,8 +6,6 @@
 #$ -S /bin/bash
 #$ -M junming_shi@berkeley.edu
 
-SIMU_NUM = 1
-
 for SAMPLE_N in 200 500 1000; do
 
     HALF1 = FALSE
@@ -16,7 +14,7 @@ for SAMPLE_N in 200 500 1000; do
     FIRST_SMALLKNOTS = TRUE
 
     # Export the variables
-    export SIMU_NUM SAMPLE_N HALF1 HALF2 GRID_EXTRA FIRST_SMALLKNOTS
+    export SIMU_NUM=1 SAMPLE_N HALF1 HALF2 GRID_EXTRA FIRST_SMALLKNOTS
     
     R CMD BATCH --no-save run_simu/run_simu.R "run_simu/simu_${SIMU_NUM}_${SAMPLE_N}_first_smallKnots.Rout"
 
