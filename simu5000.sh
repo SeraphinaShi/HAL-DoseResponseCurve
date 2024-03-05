dfc695f845fd475201cb1055e82f1bb09be30d55
SeraphinaShi <junmingshi67@gmail.com>
2024-03-05 11:42:47 -0800

    add simulations with n=5000

--- simu5000.sh
+++ simu5000.sh
@@ -18,13 +18,5 @@ export SIMU_NUM=2 SAMPLE_N=5000 PART1=True PART2=False PART3=False; R CMD BATCH 
 export SIMU_NUM=2 SAMPLE_N=5000 PART1=False PART2=True PART3=False; R CMD BATCH --no-save run_simu/run_simu.R "run_simu/simu_${SIMU_NUM}_${SAMPLE_N}_part2.Rout" &
 export SIMU_NUM=2 SAMPLE_N=5000 PART1=False PART2=False PART3=True; R CMD BATCH --no-save run_simu/run_simu.R "run_simu/simu_${SIMU_NUM}_${SAMPLE_N}_part3.Rout" &
 
-export SIMU_NUM=3 SAMPLE_N=5000 PART1=True PART2=False PART3=False; R CMD BATCH --no-save run_simu/run_simu.R "run_simu/simu_${SIMU_NUM}_${SAMPLE_N}_part1.Rout" &
-export SIMU_NUM=3 SAMPLE_N=5000 PART1=False PART2=True PART3=False; R CMD BATCH --no-save run_simu/run_simu.R "run_simu/simu_${SIMU_NUM}_${SAMPLE_N}_part2.Rout" &
-export SIMU_NUM=3 SAMPLE_N=5000 PART1=False PART2=False PART3=True; R CMD BATCH --no-save run_simu/run_simu.R "run_simu/simu_${SIMU_NUM}_${SAMPLE_N}_part3.Rout" &
-
-export SIMU_NUM=4 SAMPLE_N=5000 PART1=True PART2=False PART3=False; R CMD BATCH --no-save run_simu/run_simu.R "run_simu/simu_${SIMU_NUM}_${SAMPLE_N}_part1.Rout" &
-export SIMU_NUM=4 SAMPLE_N=5000 PART1=False PART2=True PART3=False; R CMD BATCH --no-save run_simu/run_simu.R "run_simu/simu_${SIMU_NUM}_${SAMPLE_N}_part2.Rout" &
-export SIMU_NUM=4 SAMPLE_N=5000 PART1=False PART2=False PART3=True; R CMD BATCH --no-save run_simu/run_simu.R "run_simu/simu_${SIMU_NUM}_${SAMPLE_N}_part3.Rout" &
-
 # Wait for all background jobs to finish
 wait
